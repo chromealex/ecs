eb9eab8584029ecf5435dae26c578e63f5a9a36f
Alex Feer <chrome.alex@gmail.com>
2020-02-23 14:35:12 +0300

    Removed unused files

--- Assets/ECS/Example/Scripts/Components/Points/PointAddPositionDelta.cs
+++ Assets/ECS/Example/Scripts/Components/Points/PointAddPositionDelta.cs
@@ -4,7 +4,6 @@ namespace ME.Example.Game.Components {
 
     using ME.Example.Game.Entities;
 
-    [Unity.Burst.BurstCompileAttribute]
     public class PointAddPositionDelta : IComponentOnce<State, Point> {
 
         public UnityEngine.Vector3 positionDelta;

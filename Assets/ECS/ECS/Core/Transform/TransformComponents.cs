0153eb5a7fd09e41c781655ef69fa3af2dea1fa7
Alex Feer <chrome.alex@gmail.com>
2020-05-06 13:46:37 +0300

    Core: - Transform utils added

---
+++ Assets/ECS/ECS/Core/Transform/TransformComponents.cs
@@ -0,0 +1,40 @@
+﻿namespace ME.ECS.Transform {
+
+    public struct Container : IStructComponent {
+
+        public Entity entity;
+
+    }
+
+    public struct Childs : IStructComponent {
+
+        public ME.ECS.Collections.StackArray50<Entity> childs;
+
+    }
+
+    public struct Position : IStructComponent {
+
+        public float x;
+        public float y;
+        public float z;
+
+    }
+    
+    public struct Rotation : IStructComponent {
+
+        public float x;
+        public float y;
+        public float z;
+        public float w;
+
+    }
+    
+    public struct Scale : IStructComponent {
+
+        public float x;
+        public float y;
+        public float z;
+
+    }
+    
+}
